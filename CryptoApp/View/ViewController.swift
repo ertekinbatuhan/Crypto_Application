9c4922dd1b26083b0e0be553a02ab5b75667d2e9
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2023-09-21 14:24:35 +0300

    search bar added

--- CryptoApp/View/ViewController.swift
+++ CryptoApp/View/ViewController.swift
@@ -9,16 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     let url = URL(string:"http://data.fixer.io/api/latest?access_key=798c8fae3cd7d13011fca8fa483039f3")
-    var cryptoArray  = [Crypto]()
+    var cryptoArray  : [Crypto] = [Crypto] ()
+    var searchList  : [Crypto] = [Crypto] ()
+    
+    var isSearching = false
   
     override func viewDidLoad() {
         super.viewDidLoad()
        
         tableView.dataSource = self
         tableView.delegate = self
+        searchBar.delegate = self
         
         let session = URLSession.shared
         let task = session.dataTask(with: url!) { data, response, error in
@@ -67,23 +72,43 @@ class ViewController: UIViewController {
 
 extension ViewController : UITableViewDelegate , UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cryptoArray.count
+        
+        if isSearching {
+            
+            return searchList.count
+            
+        } else  {
+            return cryptoArray.count
+        }
+        
+        
+       
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell =  UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = cryptoArray[indexPath.row].cryptoName
-        content.secondaryText = String(cryptoArray[indexPath.row].cryptoPrice)
-        cell.contentConfiguration = content
+        
+       
         
         
         cell.layer.cornerRadius = 20
         cell.backgroundColor = .systemGreen
-        cell.layer.borderWidth = 2.0
+        cell.layer.borderWidth = 4.0
         cell.layer.masksToBounds = true
         cell.layer.borderColor = CGColor(gray: 5, alpha: 5)
+        
+        if isSearching {
+            content.text = searchList[indexPath.row].cryptoName
+            content.secondaryText = String(searchList[indexPath.row].cryptoPrice)
+        } else {
+            content.text = cryptoArray[indexPath.row].cryptoName
+            content.secondaryText = String(cryptoArray[indexPath.row].cryptoPrice)
+            
+        }
+        
+        cell.contentConfiguration = content
         
         return cell
     }
@@ -92,6 +117,23 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource {
         return 80
     }
     
+    
+}
+
+extension ViewController : UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText == "" {
+                  isSearching = false
+               } else {
+                   isSearching = true
+                   searchList = cryptoArray.filter({$0.cryptoName.lowercased().contains(searchText.lowercased())})
+               }
+               
+        tableView.reloadData()
+        
+    }
     
     
 }
